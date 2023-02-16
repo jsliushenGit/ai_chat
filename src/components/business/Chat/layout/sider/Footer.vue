4c34dfde800eea3f78aee7a7c33193a0a058b71e
jsliushenGit <jsliushen@163.com>
2023-02-17 00:42:10 +0800

    feat: init

---
+++ src/components/business/Chat/layout/sider/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t h-[70px]">
+    <!-- <UserAvatar class="flex-1" />
+    <HoverButton tooltip="Setting">
+      <span class="text-xl text-[#4f555e]">
+        <SvgIcon icon="ri:settings-4-line" />
+      </span>
+    </HoverButton> -->
+  </footer>
+</template>
